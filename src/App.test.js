bc7b40a39fd0ff17e77a906309d9d5eb76761f7b
vishal <vishalgheravada@atyantik.com>
2020-02-10 18:51:47 +0530

    Babel config & set up

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import Enzyme, {shallow} from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[dataTest='AppComponent']");
+  expect(appComponent.length).toBe(1);
 });
