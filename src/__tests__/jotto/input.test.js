d26b607568cadcac264e7792874817c6902f65d2
vishal <vishalgheravada@atyantik.com>
2020-02-12 18:57:41 +0530

    add input component

---
+++ src/__tests__/jotto/input.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Input from "../../jotto/input";
+import {findByTestAttr} from "./utils";
+
+
+describe("render", () => {
+    describe("word has been guessed", () => {
+        test("renders component without error", () => {});
+        test("does not renders input box", () => {});    
+        test("does not renders submit button", () => {});
+    });
+    describe("word has not been guessed", () => {
+        test("renders component without error", () => {});
+        test("renders input box", () => {});    
+        test("renders submit button", () => {});
+    });
+});
+
+
+
+describe("update state", () => {
+    describe("word has been guessed", () => {
+        
+    });
+});
