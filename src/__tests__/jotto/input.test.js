7846f6cea952ad7607f6ad4c4f709cfb9677ad2e
vishal <vishalgheravada@atyantik.com>
2020-02-17 12:30:55 +0530

    Write test case for input success=true

--- src/__tests__/jotto/input.test.js
+++ src/__tests__/jotto/input.test.js
@@ -12,9 +12,23 @@ const init = (initialState = {}) => {
 
 describe("render", () => {
     describe("word has been guessed", () => {        
-        test("renders component without error", () => {});
-        test("does not renders input box", () => {});    
-        test("does not renders submit button", () => {});
+        let wrapper;
+        beforeEach(() => {
+            const initialState = {success: true};
+            wrapper = init(initialState);
+        });
+        test("renders component without error", () => {
+            const inputComponent = findByTestAttr(wrapper, "inputComponent");
+            expect(inputComponent.length).toBe(1);
+        });
+        test("does not renders input box", () => {
+            const inputBox = findByTestAttr(wrapper, "inputBox");
+            expect(inputBox.length).toBe(0);
+        });    
+        test("does not renders submit button", () => {
+            const submitButton = findByTestAttr(wrapper, "submitButton");
+            expect(submitButton.length).toBe(0);
+        });
     });
     describe("word has not been guessed", () => {
         let wrapper;
@@ -34,7 +48,7 @@ describe("render", () => {
         test("renders submit button", () => {
             const submitButton = findByTestAttr(wrapper, "submitButton");
             expect(submitButton.length).toBe(1);
-        }); 
+        });     
     });
 });
 
